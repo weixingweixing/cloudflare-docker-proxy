7386e17445790ef8a6d00287f7e7254127942898
Xing Wei <weix@ecdatainfo.com>
2024-06-11 14:06:48 +0800

    edit dns

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.wxssl.top": "https://registry-1.docker.io",
+  "quay.wxssl.top": "https://quay.io",
+  "gcr.wxssl.top": "https://gcr.io",
+  "k8s-gcr.wxssl.top": "https://k8s.gcr.io",
+  "k8s.wxssl.top": "https://registry.k8s.io",
+  "ghcr.wxssl.top": "https://ghcr.io",
+  "cloudsmith.wxssl.top": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
